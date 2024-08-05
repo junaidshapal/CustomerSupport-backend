a1ba113d8539004dd077395d30ec2f77ffe3a2c4
Junaid <mughaljunaidmughal136@gmail.com>
2024-08-06 01:18:17 +0500

    New changes

---
+++ CustomerSupportAPI/Models/TicketComment.cs
@@ -0,0 +1,14 @@
+﻿namespace CustomerSupportAPI.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public string CommentMessage { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string ModifiedBy { get; set; }
+        public DateTime ModifiedOn { get; set; }
+
+    }
+}
