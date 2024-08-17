9b5e180cdfd63e0ccb14c7ac205f9c79405f8134
Junaid <mughaljunaidmughal136@gmail.com>
2024-08-18 00:20:52 +0500

    Hello

---
+++ CustomerSupportAPI/Models/TicketHistory.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerSupportAPI.Models
+{
+    public class TicketHistory
+    {
+        public int Id { get; set; }
+        public int TicketId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string JsonData { get; set; } = string.Empty;
+
+    }
+}
