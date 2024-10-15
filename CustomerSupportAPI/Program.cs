9c2e67e6696191259b38f281e5f4ad6a94704cc5
Junaid <mughaljunaidmughal136@gmail.com>
2024-10-15 22:19:02 +0500

    More changes

--- CustomerSupportAPI/Program.cs
+++ CustomerSupportAPI/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(connectionString));
-
+    
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
