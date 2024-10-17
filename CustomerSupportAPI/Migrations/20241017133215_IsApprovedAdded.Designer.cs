5690f58221a0db3ee4d06e420a7f128cacd3929b
Junaid <mughaljunaidmughal136@gmail.com>
2024-10-17 18:37:34 +0500

    IsApproved added to database

--- CustomerSupportAPI/Migrations/20241017133215_IsApprovedAdded.Designer.cs
+++ CustomerSupportAPI/Migrations/20241017133215_IsApprovedAdded.Designer.cs
@@ -4,6 +4,7 @@ using CustomerSupportAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CustomerSupportAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241017133215_IsApprovedAdded")]
+    partial class IsApprovedAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
